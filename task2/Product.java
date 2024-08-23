cf1b9c4799f885491a0e30907eaf54435142e9cd
Vishalini-Dev <140333684+Vishalini-Dev@users.noreply.github.com>
2024-08-23 20:01:53 +0530

    Update Product.java

--- task2/Product.java
+++ task2/Product.java
@@ -2,25 +2,27 @@
 
 package guvi.task2;
 import java.util.Scanner;
-
+// create product class
 public class Product {
     int pid;
     float price;
     int quantity;
-
+// constructor
     Product(int pid, float price, int quantity) {
         this.pid = pid;
         this.price = price;
         this.quantity = quantity;
     }
 }
+//create xyz class
 class XYZ {
 
     public static void main(String[] args) {
-
+        // array
         Product[] products = new Product[5];
 
         Scanner scanner = new Scanner(System.in);
+        // get input and store in product array
         for (int i = 0; i <5; i++) {
             System.out.println("Enter pid, price, and quantity for product: ") ;
             int pid = scanner.nextInt();
@@ -29,7 +31,7 @@ class XYZ {
             products[i] = new Product(pid, price, quantity);
         }
 
-
+         // check highest price product and print it
         float maxPrice = products[0].price;
         int maxPid = products[0].pid;
         for (int i = 1; i < 5; i++) {
@@ -39,12 +41,12 @@ class XYZ {
             }
         }
         System.out.println("Product with highest price has pid: " + maxPid);
-
+         // print the total product price
         float totalAmount = calculateTotalAmount(products);
         System.out.println("Total amount spent on all products: " + totalAmount);
     }
 
-
+     // sum of total product price
     public static float calculateTotalAmount(Product[] products) {
         float total = 0.0f;
         for ( Product product : products) {
