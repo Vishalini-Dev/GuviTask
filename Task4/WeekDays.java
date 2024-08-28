7158068d1d59ed17d4771eccb41d4a52cc391267
Vishalini-Dev <140333684+Vishalini-Dev@users.noreply.github.com>
2024-08-28 17:30:45 +0530

    Update WeekDays.java

--- Task4/WeekDays.java
+++ Task4/WeekDays.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class WeekDays {
     public static void main(String[] args) {
+        //array
         String[] weekdays={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-        //System.out.println(days.length);
         Scanner scan =new Scanner(System.in);
         System.out.println("Enter the day position from 0 to 6");
         int num= scan.nextInt();
         try{
+            //print name of the day 
             System.out.println(weekdays[num]);
         }catch (IndexOutOfBoundsException e){
             System.out.println("Day position is outside the range (0-6)");
