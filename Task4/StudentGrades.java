7303080f1411ea821636742a56af3c70363216a8
Vishalini-Dev <140333684+Vishalini-Dev@users.noreply.github.com>
2024-08-28 20:05:51 +0530

    Update StudentGrades.java

--- Task4/StudentGrades.java
+++ Task4/StudentGrades.java
@@ -3,36 +3,40 @@
 import java.util.HashMap;
 import java.util.Scanner;
 public class StudentGrades {
+    //create Hash map
     private HashMap<String, Integer> grades;
 
+    //create constructor
     public StudentGrades() {
         this.grades = new HashMap<>();
     }
-
+    // add elements on hash map
     public void addStudent(String name, int grade) {
         grades.put(name, grade);
     }
-
+    // remove elements on hash map
     public void removeStudent(String name) {
         grades.remove(name);
     }
 
+    // get the mark for name 
     public Integer getGrade(String name) {
         return grades.get(name);
     }
-
+    //display name and mark
     public void display() {
         for (String name : grades.keySet()) {
             System.out.println("Student: " + name + ", Grade: " + grades.get(name));
         }
     }
-
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         StudentGrades studentGrades = new StudentGrades();
         String name;
         int grade;
 
+        //add the name and grades using addStudent method
         while (true) {
             System.out.println("Enter a name (or type 'exit' to finish adding):");
             name = scan.nextLine();
@@ -46,14 +50,19 @@ public class StudentGrades {
             studentGrades.addStudent(name, grade);
         }
 
+        
         System.out.println("Would you like to remove a student? (yes/no)");
         String response = scan.nextLine();
-
+         //remove student name and grade using removeStudent method
         if (response.equalsIgnoreCase("yes")) {
             System.out.println("Enter the name of the student to remove:");
             String nameToRemove = scan.nextLine();
             studentGrades.removeStudent(nameToRemove);
         }
+        //display the all name and grades in hash map
+        studentGrades.display();
+    }
+}
 
         studentGrades.display();
     }
